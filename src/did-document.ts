5861bb848af3e99ab92ef54f95dae7f8651a44db
silentcastledev <dd9efe7fb5c01@protonmail.com>
2020-11-18 15:33:01 +0400

    Return DID Resolution

---
+++ src/did-document.ts
@@ -0,0 +1,46 @@
+export interface DIDDocument {
+    "@context": "https://w3id.org/did/v1";
+    id: string;
+    publicKey: PublicKey[];
+    authentication?: (string | PublicKey | Authentication)[];
+    uportProfile?: any;
+    service?: ServiceEndpoint[];
+    created?: string;
+    updated?: string;
+    proof?: LinkedDataProof;
+    keyAgreement?: (string | PublicKey)[];
+    assertionMethod?: (string | PublicKey)[];
+    capabilityDelegation?: (string | PublicKey)[];
+    capabilityInvocation?: (string | PublicKey)[];
+}
+
+export interface Authentication {
+    type: string;
+    publicKey: string;
+}
+
+export interface PublicKey {
+    id: string;
+    type: string;
+    controller: string;
+    ethereumAddress?: string;
+    publicKeyBase64?: string;
+    publicKeyBase58?: string;
+    publicKeyHex?: string;
+    publicKeyPem?: string;
+}
+
+export interface ServiceEndpoint {
+    id: string;
+    type: string;
+    serviceEndpoint: string;
+    description?: string;
+}
+
+export interface LinkedDataProof {
+    type: string;
+    created: string;
+    creator: string;
+    nonce: string;
+    signatureValue: string;
+}
--- src/remote-did-resolver.ts
+++ src/remote-did-resolver.ts
@@ -1,6 +1,6 @@
-import {DIDDocument} from "did-resolver";
 import {NoResolutionError} from "./no-resolution.error";
 import fetch from "cross-fetch";
+import {DIDResolution} from "./did-resolution";
 
 /**
  * Resolve by asking Uniresolver for DID Document.
@@ -14,11 +14,10 @@ export class RemoteDidResolver {
         this.endpoint = `${noTrailingSlash}/1.0/identifiers`
     }
 
-    async resolve(didUrl: string): Promise<DIDDocument> {
+    async resolve(didUrl: string): Promise<DIDResolution> {
         const response = await fetch(`${this.endpoint}/${didUrl}`);
         if (response.status >= 200 && response.status < 300) {
-            const didResolution = await response.json();
-            return didResolution.didDocument
+            return await response.json();
         } else {
             throw new NoResolutionError(didUrl);
         }
